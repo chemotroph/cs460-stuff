30a89e1dfe09c0c72632aea30620ec67e9fa5764
chemotroph <zakkeipp@gmail.com>
2019-10-12 15:02:22 -0700

    Started working on minesweeper, setting up javascript to dynamically create a grid based on user input.

--- HW2/page2.js
+++ HW2/page2.js
@@ -1,9 +1,26 @@
 
 
 
-document.getElementById("singlebutton").addEventListener("click", myFunction);
+document.getElementById("submit").addEventListener("click", generate);
 var count=0;
 
-function myFunction() {
-  document.getElementById("singlebutton").innerHTML = "YOU CLICKED ME "+(count++)+ " TIMES!!!";
+function generate()
+{
+
+  var height=document.getElementById("height").value;
+  var width=document.getElementById("width").value;
+  document.getElementById("test").innerHTML = width+", "+height;
+  var outputhtml;
+  for(i=0;i<heighti++)
+  {
+    outputhtml=outputhtml+"<row>";
+    for(j=0;j<width;j++)
+    {
+      outputhtml=outputhtml+"<col>"+" pooppp";
+      outputhtml=outputhtml+"</col>";
+
+    }
+    outputhtml=outputhtml+"</row>";
+  }
+  document.getElementById("test").innerHTML=outputhtml
 }
